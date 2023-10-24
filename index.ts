0a78c8c31762725c4b16dd9619617377c74a733b
Adrien Berger <adrienberger66@gmail.com>
2023-10-24 12:51:01 +0000

    test Request

--- index.ts
+++ index.ts
@@ -1,7 +1,23 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
-  return new Response("Hello World");
+    const body = `{
+        sim1: "patate",
+        sim2: "patate",
+        lang: "fr",
+        type: "General Word2Vec"
+      }`;
+    const resp = await fetch("http://nlp.polytechnique.fr/similarityscore", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body,
+        })
+        .then((resp : any) => {
+            const score = resp.json().score;
+        }) 
+    return new Response(JSON.stringify(_req));
 }
 
 serve(handler);
